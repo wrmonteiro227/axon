557afe90427225f31fd535dd84a9fa5690997bad
wrmonteiro227 <wrdd3000@gmail.com>
2026-03-07 01:25:18 -0300

    Update sw.js

--- axon/sw.js
+++ axon/sw.js
@@ -1,20 +1,7 @@
-const CACHE_NAME = 'axon-v1';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
